8eacfd890d14c9ce8a31bd9d0b6b124c5ebb9cbf
Aluno <claudiolopes.sistemas@gmail.com>
2018-09-27 22:28:01 -0300

    aula de 27 de setembro

--- 3 trimestre/aula28.py
+++ 3 trimestre/aula28.py
@@ -1,84 +1,92 @@
-# importa uma biblioteca para poder usar o seu conteudo
-import  sqlite3
-###############FUNÇÕES##########
+# Importa uma biblioteca para poder usar seu conteúdo
+import sqlite3
 
-def criar_tabela_usuario(cursor):
+########### F U N Ç Õ E S #########
 
+def criar_tabela_usuario(conexao):
 
-#monta o sql a  ser executado
+    # Cria o cursor para operar o banco
+    cursor = conexao.cursor()
+
+    # Monta o SQL a ser executado
     sql = """
-        CREATE table IF NOT EXISTS usuario(
-            nome text,
-            login text,
-            senha text
-            );
-        """
-        #EXECULTA UM sql
-        cursor.execute(sql)
+    CREATE TABLE IF NOT EXISTS usuario(
+    nome text,
+    login text,
+    senha text
+    );
+    """
 
+    # Executa um SQL
+    cursor.execute(sql)
+
+    # Inserir novo usuário
 def inserir_usuario(conexao):
 
+    # Cria o cursos para operar o banco
+    cursor = conexao.cursor()
 
-    #cria o cursor para operar o banco
-    cursor+conexao.cursor()
-
-
-
-#################################
-#iniciar uma conexão (ligação com nosso) banco
-conexao = sqlite3.connect("aula28.sqlite")
-
-#2 - Definir um meio de operar/exe
-#Seria como se ele enviasse
-cursor = conexao.cursor()
-
-###########Criação de um banco de dados ###################
-
-
-
-############### Inserindo um registro####################
-nome = input ("nome:")
-login = input ("login:")
-senha = input ("senha:")
-
-sql = """
-    INSERT INTO usuario VALUES (
-        '{}',
-        '{}',
-        '{}'
+    # Pede os dados para o usuário
+    nome = input("Nome: ")
+    login = input("Login: ")
+    senha = input("Senha: ")
+    
+    # Monta o select com o format e os dados
+    sql = """
+    INSERT INTO usuario VALUES(
+    '{}',
+    '{}',
+    '{}'
     );
-""".format(nome, login, senha)
-#Executa o sql
-cursor.execute(sql)
+    """.format(nome, login, senha)
 
-#Salvar as modificações.
-#O commit sempre deve ser feito apos a inserção no banco
-conexao.commit()
+    # Exemplo:
+    # sql = """
+    #     INSERT INTO usuario VALUES(
+    #         'Rafael Zottesso',
+    #         'rafael',
+    #         '123'
+    #     );
+    # """
+
+    # Executa o sql
+    cursor.execute(sql)
+
+    # Salvar as modificações.
+    # O commit sempre deve ser feito depois do INSERT, UPDATE ou DELETE
+    conexao.commit()
+
+        # Listar os registros de usuário
+        def listar_usuarios(conexao):
+
+            # Cria o cursor para operar o banco
+            cursor = conexao.cursor()
+
+            # Monta o SQL
+            sql = "SELECT rowid, * FROM usuario;"
+
+            # Executa o SQL
+            cursor.execute(sql)
+
+            # Armazena os dados (registros) do select
+            # Toda vez que executa o select, precisa usar o fetch para buscar os registros
+            usuarios = cursor.fetchall() # buscar todos
+
+            # Percorrer a lista com os registros
+            # Estou chamando de "u" cada item dessa lista
+            for usr in usuarios:
+                print( "{}: {} ({})".format(usr[0], usr[1], usr[2]) )
+
+                ############ P R I N C I P A L #############
+
+                # 1º - Iniciar a conexão (ligação) com nosso banco
+                print("Conectando no banco...\n\n")
+                conexao = sqlite3.connect("aula28.sqlite")
 
 
+                listar_usuarios(conexao)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Fechando a conexão
-conexao.close()
+                # Fechando a conexão (ligação) com o banco
+                print("\n\nFechando conexão com o banco...")
+                conexao.close()
